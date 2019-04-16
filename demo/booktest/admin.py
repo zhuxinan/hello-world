d484ce242c1ef31da98c3ffbc9569d056e4fccb2
zhuxinan <18238955523qq>
2019-04-16 19:10:11 +0800

    数据迁移、测试，站点操作

--- demo/booktest/admin.py
+++ demo/booktest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookInfo,HeroInfo
 
 # Register your models here.
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
--- demo/booktest/models.py
+++ demo/booktest/models.py
@@ -5,11 +5,15 @@ from django.db import models
 class BookInfo(models.Model):
     bname = models.CharField(max_length=20)
     bpub_date = models.DateTimeField()
+    def __str__(self):
+        return self.bname
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=100)
     hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.hname
 
 
