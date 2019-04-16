9d7070b086873291c0a367a3c2631a868be3ca0f
zhuxinan <18238955523qq>
2019-04-16 11:55:35 +0800

    创建模型

--- demo/booktest/models.py
+++ demo/booktest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    bname = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
+
+
