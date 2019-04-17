8e5adb124f49a4da72f47a3efe2624c63b17941b
zhuxinan <18238955523qq>
2019-04-17 19:00:37 +0800

    模板操作

--- demo/booktest/views.py
+++ demo/booktest/views.py
@@ -3,16 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import BookInfo,HeroInfo
+from django.template import loader,RequestContext
 
 def index(request):
-    return HttpResponse('首页')
+    # return HttpResponse('首页')
+    template = loader.get_template('booktest/index.html')
+    cont = {"username":"admin"}
+    result = template.render(cont)
+    return HttpResponse(result)
+
 
 def list(request):
-    return HttpResponse('列表页')
+    # return HttpResponse('列表页')
+    book = BookInfo.objects.all()
+    return render(request,'booktest/list.html',{"booklist":book})
 
 def detail(request,id):
-    try:
-        book = BookInfo.objects.get(pk=int(id))
-        return HttpResponse('detail %s' %book)
-    except:
-        return HttpResponse('id不对，请重新输入')
+    # try:
+    #     book = BookInfo.objects.get(pk=int(id))
+    #     return HttpResponse('detail %s' %book)
+    # except:
+    #     return HttpResponse('id不对，请重新输入')
+    book = BookInfo.objects.get(pk=id)
+    return render(request,'booktest/detail.html',{"book":book})
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('booktest/', include('booktest.urls'))
+    url('booktest/', include('booktest.urls',namespace='booktest')),
 ]
